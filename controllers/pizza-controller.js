b63e90b76792748ced2ff6fc706f6f63d7823f4b
Lakyn <lakynmphotography@hotmail.com>
2021-09-15 22:53:26 -0400

    finished feature/user-models feature

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,43 +1,35 @@
-const { pizza } = require('../models/');
+const { Pizza } = require('../models');
 
 const pizzaController = {
-    // get all pizzas
-    getAllPizza(req, res) {
-      Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
-  
-    // get one pizza by id
-    getPizzaById({ params }, res) {
-      Pizza.findOne({ _id: params.id })
-        .then(dbPizzaData => {
-          // If no pizza is found, send 404
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
-  
-// createPizza
-createPizza({ body }, res) {
-    Pizza.create(body)
+  // get all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
       .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => res.status(400).json(err));
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
   },
 
+  // get one pizza by id
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 
-// update pizza by id
-updatePizza({ params, body }, res) {
+  // createPizza
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  },
+
+  // update pizza by id
+  updatePizza({ params, body }, res) {
     Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
@@ -49,8 +41,8 @@ updatePizza({ params, body }, res) {
       .catch(err => res.status(400).json(err));
   },
 
-// delete pizza
-deletePizza({ params }, res) {
+  // delete pizza
+  deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
@@ -61,5 +53,6 @@ deletePizza({ params }, res) {
       })
       .catch(err => res.status(400).json(err));
   }
-}
+};
+
 module.exports = pizzaController;
--- models/pizza.js
+++ models/pizza.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-
 const PizzaSchema = new Schema({
   pizzaName: {
     type: String
@@ -19,8 +18,6 @@ const PizzaSchema = new Schema({
   toppings: []
 });
 
-// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
